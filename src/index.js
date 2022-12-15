f3ab43a8baa4161958a34dba3a136c0ccf11957c
Gankzi <hoonkang1024@gmail.com>
2022-12-14 22:15:17 -0800

    Add hard code html/css to visualize website
    Added hard code to html/css to visualize the look of the
    website.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,10 @@ function component () {
 }
 
 content.append(component());
+
+
+// Add Project button will ask for name of the project and creates a project
+// Project will have add todo list button that will call a form to create a new todo Class
+// new todo class will be pushed to todo array for each project.
+// Dom Manipulation function will use the todo array and the todo classes on to the screen.
+// 
